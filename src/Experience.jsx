d2577c9305450533b2b7f8012de0a1dc66082408
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-02 18:53:22 +0100

    add contact shadow

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import {Environment, Float, PresentationControls, useGLTF} from '@react-three/drei';
+import {ContactShadows, Environment, Float, PresentationControls, useGLTF} from '@react-three/drei';
 
 export default function Experience() {
     const computer = useGLTF('./macbook_model.gltf');
@@ -18,5 +18,11 @@ export default function Experience() {
                 <primitive object={computer.scene} position-y={-1.2}/>
             </Float>
         </PresentationControls>
+        <ContactShadows
+            position-y={-1.4}
+            opacity={0.4}
+            scale={5}
+            blur={2.4}
+        />
     </>
 }
