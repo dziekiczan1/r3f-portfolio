3a9f0d5efe5fa02a91d574592fcd3ffc110a0e4c
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-02 18:39:15 +0100

    add model and environment

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,14 +1,12 @@
-import {OrbitControls} from '@react-three/drei';
+import {Environment, OrbitControls, useGLTF} from '@react-three/drei';
 
 export default function Experience() {
+    const computer = useGLTF('./macbook_model.gltf');
+
     return <>
-
+        <Environment preset={'city'}/>
+        <color args={['#241a1a']} attach="background"/>
         <OrbitControls makeDefault/>
-
-        <mesh>
-            <boxGeometry/>
-            <meshNormalMaterial/>
-        </mesh>
-
+        <primitive object={computer.scene}/>
     </>
 }
