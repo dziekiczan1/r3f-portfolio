e474b747e157ff9992584eed119cd7aa6c2c4a59
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-02 18:51:56 +0100

    add presentation controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import {Environment, OrbitControls, useGLTF} from '@react-three/drei';
+import {Environment, Float, PresentationControls, useGLTF} from '@react-three/drei';
 
 export default function Experience() {
     const computer = useGLTF('./macbook_model.gltf');
@@ -6,7 +6,17 @@ export default function Experience() {
     return <>
         <Environment preset={'city'}/>
         <color args={['#241a1a']} attach="background"/>
-        <OrbitControls makeDefault/>
-        <primitive object={computer.scene}/>
+        <PresentationControls
+            global
+            rotation={[0.13, 0.1, 0]}
+            polar={[-0.4, 0.2]}
+            azimuth={[-1, 0.75]}
+            config={{mass: 2, tension: 400}}
+            snap={{mass: 4, tension: 400}}
+        >
+            <Float rotationIntensity={0.4}>
+                <primitive object={computer.scene} position-y={-1.2}/>
+            </Float>
+        </PresentationControls>
     </>
 }
